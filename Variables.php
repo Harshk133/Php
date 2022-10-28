28bed6b5836f92ced1e3aab07168acd9b4dfcfed
Harsh Moreshwar Kale <harshmkale.2004@gmail.com>
2022-10-28 20:48:42 +0530

    Initial Commit!

---
+++ Variables.php
@@ -0,0 +1,15 @@
+
+<?php
+// echo "We are in Variables.php";
+// Variables are containers for storing information.
+// starts with a dollar $.
+$name = "Harsh Kale";
+$income = 200;
+$isProgrammer = true;
+
+echo "This guy is $name and his income is Rs. $income <br>";
+
+echo "$name is a good boy <br>";
+echo "$name is a Hacker <br>";
+echo "<i>Harsh</i> is Programmer the answer is $isProgrammer"
+?>
