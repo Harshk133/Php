cfe87178c2c6297be6f05e7bc0622f5638d5493e
Harsh Moreshwar Kale <harshmkale.2004@gmail.com>
2022-10-31 20:57:29 +0530

    Commit #1

--- operators.php
+++ operators.php
@@ -1,4 +1,19 @@
 <?php
 // Operators in Php
+// 1. Arithmetic Operators
+// 2. Assignment Operators
+// 3. Comparision Operators
+// 4. Logical Operators
+
+$a = 45;
+$b = 8;
+
+// 1. Arithmetic Operators
+echo "For a + b, result is " . ($a + $b) . "<br>";
+echo "For a - b, result is " . ($a - $b) . "<br>";
+echo "For a * b, result is " . ($a * $b) . "<br>";
+echo "For a / b, result is " . ($a / $b) . "<br>";
+echo "For a % b, result is " . ($a % $b) . "<br>";
+echo "For a ** b, result is " . ($a ** $b) . "<br>";
 
 ?>
